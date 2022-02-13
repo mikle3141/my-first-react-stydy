6b7766f6f580d4c8195030d9f68eed42bc45a2ca
mikle <mikle314@list.ru>
2022-02-13 22:30:04 +0300

    Показать disabled button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 ReactDOM.render(
-    <App />,
+    React.createElement('button', {
+        disabled: true
+    }, 'Нажми меня!'),
   document.getElementById('root')
 );
