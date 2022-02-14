cd389ea69558846ab85c64fc68a6bc7bec7de462
mikle <mikle314@list.ru>
2022-02-14 09:30:07 +0300

    Перенести likes в компонент Счетчик

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
+import Counter from "./components/Counter";
 
 function App() {
-    const [likes, setLikes] = useState(5);
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
-    function increment() {
-        setLikes(likes + 1)
-    }
-    function decrement() {
-        setLikes(likes - 1)
-    }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-          type='text'
-          value={value}
-          onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
