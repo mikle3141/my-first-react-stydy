9085179e6fff249a593b9413fa353f0c7546601e
mikle <mikle314@list.ru>
2022-02-14 09:16:03 +0300

    Еще одно состояние, на этот раз в управляемом инпуте

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 function App() {
     const [likes, setLikes] = useState(5);
+    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
     function increment() {
         setLikes(likes + 1)
     }
@@ -11,6 +12,12 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+          type='text'
+          value={value}
+          onChange={event => setValue(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
