d5bb5d851d602d8fc4526dd386c1210229618bf1
mikle <mikle314@list.ru>
2022-02-19 23:41:30 +0300

    Классовый компонент

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   return (
     <div className="App">
       <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
