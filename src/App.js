84ee6e4ebab390ea3e79f3edf65ab1853cd3f1dd
mikle <mikle314@list.ru>
2022-03-14 01:00:45 +0300

    Использование состояния для перекладки в props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles/App.css'
 import PostItem from "./components/PostItem";
 
 function App() {
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'Javascript', body: 'Description'},
+      {id: 2, title: 'Javascript 2', body: 'Description'},
+      {id: 3, title: 'Javascript 3', body: 'Description'}
+  ])
   return (
     <div className="App">
-        <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{id: 2, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{id: 3, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{id: 4, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{id: 5, title: 'Javascript', body: 'Description'}} />
+        <h1 style={{textAlign: 'center'}}>
+            Список постов
+        </h1>
+        {posts.map(post =>
+            <PostItem post={post} key={post.id}/>
+        )}
     </div>
   );
 }
